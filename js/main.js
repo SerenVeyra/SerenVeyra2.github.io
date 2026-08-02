bd5781726e71f126b420c248bbafbfa4fe30790f
SerenVeyra <1792877971@qq.com>
2026-08-02 16:47:42 +0900

    Update main.js
    原路径识别错误，已修改路径自动识别当前网站根目录

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+// 自动获取网站根目录
+const siteRoot = window.location.pathname.split('/')[1];
 
 // 加载公共组件
-
 function loadComponent(id, file){
 
-fetch(file)
+fetch(`/${siteRoot}/${file}`)
 
 .then(response => response.text())
 
@@ -11,23 +12,23 @@ fetch(file)
 
 document.getElementById(id).innerHTML = data;
 
-});
+})
 
+.catch(error => {
+  
+console.log("组件加载失败:", error);
+
+});
+  
 }
 
-
-
 // 加载 Header
-
 loadComponent(
 "header-placeholder",
 "components/header.html"
 );
 
-
-
 // 加载 Sidebar
-
 loadComponent(
 "sidebar-placeholder",
 "components/sidebar.html"
